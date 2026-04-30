bdefcdf66904f1a33cd04d4ccde76af899f368d3
jromeroc99-rgb <jromero@keyter.es>
2026-04-30 16:50:05 +0200

    Agrega poblado inicial db

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
@@ -10,6 +11,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 app.UseRequestLocalization("en-US", "fr-FR");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
